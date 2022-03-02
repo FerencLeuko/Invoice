8b9607ff0aabfef16a7aa7a319993ae88d7a2bd9
Ferenc Leuko <fleuko@een.com>
2022-03-02 09:30:31 +0100

    change in post invoice response body

--- src/main/java/com/example/invoice/controller/InvoiceController.java
+++ src/main/java/com/example/invoice/controller/InvoiceController.java
@@ -1,6 +1,8 @@
 package com.example.invoice.controller;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Properties;
 import java.util.stream.Collectors;
 
 import javax.validation.Valid;
@@ -14,9 +16,11 @@ import com.example.invoice.exception.InvoiceValidationException;
 import com.example.invoice.exception.UnsupportedParameterException;
 import com.example.invoice.service.InvoiceService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,11 +28,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Controller
 @RequiredArgsConstructor
 public class InvoiceController
 {
+	private static final String PATH = "localhost:8081";
+	
 	@GetMapping( "/allInvoices" )
 	@ResponseBody
 	public List<InvoiceBean> getAllInvoices( )
@@ -53,7 +60,7 @@ public class InvoiceController
 	
 	@PostMapping( "/invoice" )
 	@ResponseBody
-	public InvoiceBean postInvoice(@Valid @RequestBody InvoiceCreate invoiceCreate, BindingResult errors ) throws RuntimeException
+	public ResponseEntity<InvoiceBean> postInvoice(@Valid @RequestBody InvoiceCreate invoiceCreate, BindingResult errors ) throws RuntimeException
 	{
 		if (errors.hasErrors()) {
 			throw new ValidationException( String.valueOf( errors.getAllErrors()
@@ -62,7 +69,11 @@ public class InvoiceController
 					.collect( Collectors.toList())));
 		}
 		
-		return _invoiceService.createInvoice( invoiceCreate );
+		InvoiceBean invoice = _invoiceService.createInvoice( invoiceCreate );
+		URI location = URI.create( PATH + "/invoice/?invoiceId=" + invoice.getId() );
+		
+		return ResponseEntity.created( location )
+				.body( invoice );
 	}
 	
 	@ExceptionHandler(  { UnsupportedParameterException.class } )
