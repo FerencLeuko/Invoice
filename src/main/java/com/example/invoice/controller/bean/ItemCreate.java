11a9434e7189d709c79e068c4d589cb55b1f4a4e
Ferenc Leuko <fleuko@een.com>
2022-04-18 09:03:44 +0200

    change in validation of items

--- src/main/java/com/example/invoice/controller/bean/ItemCreate.java
+++ src/main/java/com/example/invoice/controller/bean/ItemCreate.java
@@ -4,13 +4,11 @@ import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
 
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.Value;
 import org.springframework.validation.annotation.Validated;
 
 @Validated
